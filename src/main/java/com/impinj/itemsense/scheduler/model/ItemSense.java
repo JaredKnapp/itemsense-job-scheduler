189933297a349f6e30750860bb7c349a271603a5
Jared Knapp <wknapp@impinj.com>
2018-07-21 15:14:03 -0400

    Set Java Version in POM, Cleanup FXML, add ItemSenseJob

--- src/main/java/com/impinj/itemsense/scheduler/model/ItemSense.java
+++ src/main/java/com/impinj/itemsense/scheduler/model/ItemSense.java
@@ -1,19 +1,57 @@
 package com.impinj.itemsense.scheduler.model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 public @Data class ItemSense {
 	private String oid;
 	private String fileName;
 
+	@Getter
+	@Setter
+	private boolean active;
+
+	@Getter
+	@Setter
 	private String name;
+	
+	@Getter
+	@Setter
 	private String url;
+	
+	@Getter
+	@Setter
 	private String username;
+	
+	@Getter
+	@Setter
 	private String password;
+	
+	@Getter
+	@Setter
 	private String token;
-	private Boolean active;
+	
+	@Getter
+	@Setter
 	private String utcOffset;
-	private List<Job> jobList;
+	
+	@Getter
+	@Setter
+	private List<ItemSenseJob> jobList;
+	
+	public List<ItemSenseJob> getActiveJobs() {
+		return (List<ItemSenseJob>) getJobs().stream().filter(itemSenseJob -> itemSenseJob.isActive()).collect(Collectors.toList());
+	}
+
+	public List<ItemSenseJob> getJobs() {
+		if (jobList == null) {
+			jobList = new ArrayList<ItemSenseJob>();
+		}
+		return jobList;
+	}
 }

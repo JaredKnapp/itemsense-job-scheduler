04b8de533d17586af9c9cac6b7c40aa2ace9f257
jskinner <jskinner@JSKINNER-01.impinj.com>
2018-07-24 18:24:03 -0700

    Added Jobs editor

--- src/main/java/com/impinj/itemsense/scheduler/controller/EditServerController.java
+++ src/main/java/com/impinj/itemsense/scheduler/controller/EditServerController.java
@@ -5,7 +5,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import com.impinj.itemsense.scheduler.model.ItemSenseConfig;
+import com.impinj.itemsense.scheduler.model.ItemSenseConfigJob;
 import java.net.URI;
+import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,7 +18,11 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Duration;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
@@ -23,30 +31,16 @@ public class EditServerController implements Initializable  {
 
 	private ItemSenseConfig configData;
 	private ConfigurationController parent;
-
-    @FXML
-    private TextField txtName;
-
-    @FXML
-    private TextField txtHostUrl;
-
-    @FXML
-    private TextField txtUserName;
-
-    @FXML
-    private PasswordField txtPassword;
-
-    @FXML
-    private TextField txtUtcOffset;
-
-    @FXML
-    private Button btnTestConnection;
-
-    @FXML
-    private Button btnSave;
-
-    @FXML
-    private Button btnCancel;
+    @FXML private TextField txtName;
+    @FXML private TextField txtHostUrl;
+    @FXML private TextField txtUserName;
+    @FXML private PasswordField txtPassword;
+    @FXML private TextField txtUtcOffset;
+    @FXML private Button btnTestConnection;
+    @FXML private Button btnSave;
+    @FXML private Button btnCancel; 
+    @FXML private TableView jobsTableView;
+    @FXML private TableColumn<ItemSenseConfigJob, Boolean> active;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -83,6 +77,15 @@ public class EditServerController implements Initializable  {
  
         alert.showAndWait();
     }
+    void load_JobsEditor() {
+        
+    }
+    
+    @FXML
+    void btnAdd_OnAction(ActionEvent event) {
+        load_JobsEditor();
+    }
+    
     @FXML
     void btnSave_OnAction(ActionEvent event) {
 		configData.setName(txtName.getText());
@@ -93,16 +96,21 @@ public class EditServerController implements Initializable  {
 		
 		parent.onSaveData(configData);
 	}
-
+    @FXML
+    void TableViewOnMouseClicked(ActionEvent event) {
+    }
+    
 	public void setData(ItemSenseConfig serverData) {
 		this.configData = serverData;
 
 		if (serverData != null) {
-			txtName.setText(serverData.getName());
-			txtHostUrl.setText(serverData.getUrl());
-			txtUserName.setText(serverData.getUsername());
-			txtPassword.setText(serverData.getPassword());
-			txtUtcOffset.setText(serverData.getUtcOffset());
+                    txtName.setText(serverData.getName());
+                    txtHostUrl.setText(serverData.getUrl());
+                    txtUserName.setText(serverData.getUsername());
+                    txtPassword.setText(serverData.getPassword());
+                    txtUtcOffset.setText(serverData.getUtcOffset());
+                    List<ItemSenseConfigJob> jobs = serverData.getJobList();
+                    jobsTableView.setItems( FXCollections.observableArrayList( serverData.getJobList()));
 		}
 	}
 

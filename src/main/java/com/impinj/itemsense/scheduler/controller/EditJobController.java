8f7badab5b825d2bfd485600e7a994deb8459659
Jared Knapp <wknapp@impinj.com>
2018-07-25 17:50:54 -0700

    Popup job editor.

--- src/main/java/com/impinj/itemsense/scheduler/controller/EditJobController.java
+++ src/main/java/com/impinj/itemsense/scheduler/controller/EditJobController.java
@@ -4,11 +4,16 @@ package com.impinj.itemsense.scheduler.controller;
 import com.impinj.itemsense.scheduler.model.ItemSenseConfigJob;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class EditJobController {
     ItemSenseConfigJob itemSenseConfigJob;
+    EditServerController parent;
+    ItemSenseConfigJob jobData;
+    
     @FXML private boolean togActive;
     @FXML private TextField txtFacility;
     @FXML private TextField txtName;
@@ -22,9 +27,26 @@ public class EditJobController {
     void initialize() {
 
     }
-    
+/*    
     void setItemSenseConfigJob(ItemSenseConfigJob itemSenseConfigJob) {
         this.itemSenseConfigJob = itemSenseConfigJob;
-        this.itemSenseConfigJob.setName(txtName);
-    }
+        //this.itemSenseConfigJob.setName(txtName);
+    }*/
+
+	public void injectParent(EditServerController editServerController) {
+		this.parent = editServerController;	
+	}
+
+	public void setData(ItemSenseConfigJob itemSenseConfig) {
+		jobData = itemSenseConfig;
+		
+		txtName.setText(itemSenseConfig.getName());
+		
+	}
+	
+	public void btnOk_OnAction(ActionEvent event) {
+		jobData.setName(txtName.getText());
+		
+		parent.onSaveData(jobData);
+	}
 }
--- src/main/java/com/impinj/itemsense/scheduler/controller/EditServerController.java
+++ src/main/java/com/impinj/itemsense/scheduler/controller/EditServerController.java
@@ -6,6 +6,8 @@ import java.util.ResourceBundle;
 
 import com.impinj.itemsense.scheduler.model.ItemSenseConfig;
 import com.impinj.itemsense.scheduler.model.ItemSenseConfigJob;
+import com.impinj.itemsense.scheduler.util.OIDGenerator;
+
 import java.net.URI;
 import java.util.List;
 import javafx.collections.FXCollections;
@@ -15,6 +17,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -33,114 +37,146 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 
-public class EditServerController implements Initializable  {
+public class EditServerController implements Initializable {
 
 	private ItemSenseConfig configData;
 	private ConfigurationController parent;
-    @FXML private TextField txtName;
-    @FXML private TextField txtHostUrl;
-    @FXML private TextField txtUserName;
-    @FXML private PasswordField txtPassword;
-    @FXML private TextField txtUtcOffset;
-    @FXML private TableView jobsTableView;
+	Stage dialogStage;
+
+	@FXML 
+	private Button btnAdd;
+	@FXML 
+	private TextField txtName;
+	@FXML
+	private TextField txtHostUrl;
+	@FXML
+	private TextField txtUserName;
+	@FXML
+	private PasswordField txtPassword;
+	@FXML
+	private TextField txtUtcOffset;
+	@FXML
+	private TableView<ItemSenseConfigJob> tblConfigJobs;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 	}
-	
-    @FXML
-    void btnCancel_OnAction(ActionEvent event) {
-    	//Just Get Out!!
-    }
-    
-    @FXML
-    void btnTestConnection_OnAction(ActionEvent event) {
-        Client client = ClientBuilder.newClient();
-        client.register(HttpAuthenticationFeature.basic(txtUserName.getText(),
-                                                        txtPassword.getText()));
-        CoordinatorApiController itemsenseCoordinatorController = new CoordinatorApiController(client, URI.create(txtHostUrl.getText()));
-        boolean success = true;
-        try { 
-            System.out.println("# configs="+ itemsenseCoordinatorController.getUserController().getUsers().size());
-        } catch (Exception e)  {
-            success = false;
-        }
-        Alert alert = new Alert(AlertType.INFORMATION);
-        alert.setTitle("Test Connection");
-        // Header Text: null
-        alert.setHeaderText(null);
-        System.out.println("Connection to: "+ txtUserName.getText() +"/"+ txtPassword.getText() + " " +
-                                  txtHostUrl.getText());
-        if (success)
-            alert.setContentText("Connection succeeded!");
-        else
-            alert.setContentText("Connection failed!");
- 
-        alert.showAndWait();
-    }
-    void load_JobsEditor(ItemSenseConfigJob itemSenseConfig)  {
-       FXMLLoader loader = new FXMLLoader();
-       loader.setLocation(getClass().getResource("/fxml/EditJob.fxml"));
-       try {
-            BorderPane popup = loader.load();
-            Scene scene = new Scene(popup);
-            EditJobController controller = loader.getController();
-            controller.setItemSenseConfigJob(itemSenseConfig);
-            Stage dialogStage = new Stage();
-            dialogStage.setTitle("Edit Job");
-            dialogStage.initModality(Modality.WINDOW_MODAL);
-            dialogStage.setScene(scene);
-            dialogStage.showAndWait();
-       } catch (Exception e) { e.printStackTrace(); }
-    }
-    
-    @FXML
-    void btnAdd_OnAction(ActionEvent event) {
-        load_JobsEditor(null);
-    }
-    
-    @FXML
-    void btnSave_OnAction(ActionEvent event) {
+
+	@FXML
+	void btnCancel_OnAction(ActionEvent event) {
+		// Just Get Out!!
+	}
+
+	@FXML
+	void btnTestConnection_OnAction(ActionEvent event) {
+		Client client = ClientBuilder.newClient();
+		client.register(HttpAuthenticationFeature.basic(txtUserName.getText(), txtPassword.getText()));
+		CoordinatorApiController itemsenseCoordinatorController = new CoordinatorApiController(client,
+				URI.create(txtHostUrl.getText()));
+		boolean success = true;
+		try {
+			System.out.println("# configs=" + itemsenseCoordinatorController.getUserController().getUsers().size());
+		} catch (Exception e) {
+			success = false;
+		}
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Test Connection");
+		// Header Text: null
+		alert.setHeaderText(null);
+		System.out.println(
+				"Connection to: " + txtUserName.getText() + "/" + txtPassword.getText() + " " + txtHostUrl.getText());
+		if (success)
+			alert.setContentText("Connection succeeded!");
+		else
+			alert.setContentText("Connection failed!");
+
+		alert.showAndWait();
+	}
+
+	@FXML
+	void btnAdd_OnAction(ActionEvent event) {
+		loadJobEditor(new ItemSenseConfigJob());
+	}
+
+	@FXML
+	void btnSave_OnAction(ActionEvent event) {
 		configData.setName(txtName.getText());
 		configData.setUrl(txtHostUrl.getText());
 		configData.setUsername(txtUserName.getText());
 		configData.setPassword(txtPassword.getText());
 		configData.setUtcOffset(txtUtcOffset.getText());
-		
+
 		parent.onSaveData(configData);
 	}
-    @FXML
-    void TableViewOnMouseClicked(MouseEvent event) {
-        if (event.getClickCount() == 2) { // double click
-            if(jobsTableView.getSelectionModel().getSelectedItem() != null) {
-                TableViewSelectionModel selectionModel = jobsTableView.getSelectionModel();
-                ObservableList selectedCells = selectionModel.getSelectedCells();
-                TablePosition tablePosition = (TablePosition) selectedCells.get(0);
-                System.out.println("Selected Value" + tablePosition);
-                load_JobsEditor(configData.getJobList().get(tablePosition.getRow()));
-            }
-            System.out.println(jobsTableView.getSelectionModel().getSelectedItem());
-        }
-    }
-    
+
+	@FXML
+	void tblConfigJobs_OnMouseClicked(MouseEvent event) {
+		// double click
+		if (event.getClickCount() == 2) {
+			if (tblConfigJobs.getSelectionModel().getSelectedItem() != null) {
+				TableViewSelectionModel<ItemSenseConfigJob> selectionModel = tblConfigJobs.getSelectionModel();
+				ItemSenseConfigJob selectedItem = selectionModel.getSelectedItem();
+				loadJobEditor(selectedItem);
+			}
+		}
+	}
+
+	private void loadJobEditor(ItemSenseConfigJob itemSenseConfig) {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/EditJob.fxml"));
+		try {
+			Parent popup = loader.load();
+
+			EditJobController controller = loader.getController();
+			controller.injectParent(this);
+			controller.setData(itemSenseConfig);
+
+			dialogStage = new Stage();
+			dialogStage.setTitle("Edit Job");
+			dialogStage.initModality(Modality.APPLICATION_MODAL);
+			dialogStage.setScene(new Scene(popup));
+			dialogStage.showAndWait();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	public void setData(ItemSenseConfig serverData) {
 		this.configData = serverData;
 
 		if (serverData != null) {
-                    txtName.setText(serverData.getName());
-                    txtHostUrl.setText(serverData.getUrl());
-                    txtUserName.setText(serverData.getUsername());
-                    txtPassword.setText(serverData.getPassword());
-                    txtUtcOffset.setText(serverData.getUtcOffset());
-                    List<ItemSenseConfigJob> jobs = serverData.getJobList();
-                    System.out.println("class"+FXCollections.observableArrayList( serverData.getJobList()).getClass());
-                    jobsTableView.setItems( FXCollections.observableArrayList( serverData.getJobList()));
+			txtName.setText(serverData.getName());
+			txtHostUrl.setText(serverData.getUrl());
+			txtUserName.setText(serverData.getUsername());
+			txtPassword.setText(serverData.getPassword());
+			txtUtcOffset.setText(serverData.getUtcOffset());
+			List<ItemSenseConfigJob> jobs = serverData.getJobList();
+			System.out.println("class" + FXCollections.observableArrayList(serverData.getJobList()).getClass());
+			tblConfigJobs.setItems(FXCollections.observableArrayList(serverData.getJobList()));
 		}
 	}
+
 
 	public void injectParent(ConfigurationController configurationController) {
 		// TODO Auto-generated method stub
 		this.parent = configurationController;
 	}
+
+
+	public void onSaveData(ItemSenseConfigJob configJobData) {
+		if (configJobData.getOid() == null) {
+			tblConfigJobs.getItems().add(configJobData);
+			configJobData.setOid(OIDGenerator.next());
+		}
+
+		dialogStage.close();
+
+		tblConfigJobs.refresh();
+	}
+
+	public void onCancel() {
+		dialogStage.close();
+	}
+
 }
